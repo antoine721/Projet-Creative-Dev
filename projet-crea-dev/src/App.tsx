b5f298e72062bfc3b5c8cb0602f8d97bbe0b3c6c
Antoine <antoine.bbrachet@gmail.com>
2025-03-07 11:54:09 +0100

    route

--- projet-crea-dev/src/App.tsx
+++ projet-crea-dev/src/App.tsx
@@ -1,19 +1,26 @@
-import "./index.css"
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home";
+import Page1 from "./pages/Page1";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className='app'>
-      <h1 className='anim title hight white'>Lorem </h1>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page1" element={<Page1 />} />
+        </Routes>
+      </Router>
+
+      {/* <h1 className="anim title hight white">Lorem </h1>
       <div className="anim middle">
-        <h1 className="ml white">ipsum</h1> 
+        <h1 className="ml white">ipsum</h1>
         <h1 className="mr">dolor</h1>
       </div>
-      <h1 className="anim title">sit amet.</h1>
+      <h1 className="anim title">sit amet.</h1> */}
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
