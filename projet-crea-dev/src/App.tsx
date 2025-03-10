f17f26c00a6352dd20ece95545a627aa34d54ecb
JulienMnt <julien.menet@edu.devinci.fr>
2025-03-10 21:36:10 +0100

    refactor: update App component structure for improved semantics

--- projet-crea-dev/src/App.tsx
+++ projet-crea-dev/src/App.tsx
@@ -4,14 +4,14 @@ import './App.css'
 function App() {
 
   return (
-    <div className='app'>
-      <h1 className='anim title hight white'>Lorem </h1>
-      <div className="anim middle">
-        <h1 className="ml white">ipsum</h1> 
-        <h1 className="mr">dolor</h1>
-      </div>
-      <h1 className="anim title">sit amet.</h1>
-    </div>
+    <h1 className='app'>
+      <span className='anim title hight white'>Lorem </span>
+      <span className="anim middle">
+        <span className="ml white">ipsum</span> 
+        <span className="mr">dolor</span>
+      </span>
+      <span className="anim title"><span className="index">sit amet.</span></span>
+    </h1>
   )
 }
 
