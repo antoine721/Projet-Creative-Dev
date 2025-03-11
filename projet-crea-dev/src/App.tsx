251b33c240571b9361c1f6e27b8d142671f0ced0
JulienMnt <julien.menet@edu.devinci.fr>
2025-03-11 20:25:53 +0100

    feat: restructure homepage layout and add new styles for improved UI

--- projet-crea-dev/src/App.tsx
+++ projet-crea-dev/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
+    <div>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
--- projet-crea-dev/src/pages/Page1.tsx
+++ projet-crea-dev/src/pages/Page1.tsx
@@ -1,7 +1,20 @@
+import "../../public/styles/pages.css";
+
 export default function Page1() {
   return (
-    <div>
-      <h1>Page 1</h1>
+    <div className="screen">
+      <div className="qc">
+        <h1 className="firsttitle">QUESTION 1</h1>
+        <div className="choices">
+          <div className="choice joy"></div>
+          <div className="choice sadness"></div>
+          <div className="choice anger"></div>
+          <div className="choice love"></div>
+        </div>
+      </div>
+      <div className="question">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, corporis.</p>
+      </div>
     </div>
   );
 }
