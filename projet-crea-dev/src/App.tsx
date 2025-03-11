b58d0bd3d99127a5ecadff74ad1fef39b64436f1
JulienMnt <julien.menet@edu.devinci.fr>
2025-03-11 20:25:24 +0100

    feat: homepage and love and anger card on page one

--- projet-crea-dev/src/App.tsx
+++ projet-crea-dev/src/App.tsx
@@ -12,13 +12,6 @@ function App() {
           <Route path="/page1" element={<Page1 />} />
         </Routes>
       </Router>
-
-      {/* <h1 className="anim title hight white">Lorem </h1>
-      <div className="anim middle">
-        <h1 className="ml white">ipsum</h1>
-        <h1 className="mr">dolor</h1>
-      </div>
-      <h1 className="anim title">sit amet.</h1> */}
     </div>
   );
 }
--- projet-crea-dev/src/pages/Home.tsx
+++ projet-crea-dev/src/pages/Home.tsx
@@ -2,9 +2,13 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <a href="/Page1">Page 1</a>
-    </div>
+    <div className="app">
+      <h1 className="anim title hight white">Lorem </h1>
+        <div className="anim middle">
+          <h1 className="ml white">ipsum</h1>
+          <h1 className="mr">dolor</h1>
+        </div>
+        <h1 className="anim title">sit amet.</h1>
+      </div>
   );
 }
