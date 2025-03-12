3e88b34a1f0fd89832319f9abd2f111f0c47f580
Antoine <antoine.bbrachet@gmail.com>
2025-03-12 19:00:34 +0100

    finis

--- projet-crea-dev/src/context/Question1.tsx
+++ projet-crea-dev/src/context/Question1.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 import { useEmotion } from "./EmotionContext";
 import Aurora from "./Aurora";
@@ -7,6 +8,33 @@ import "../../public/styles/pages.css";
 import { Emotion } from "../types";
 
 const Question1: React.FC = () => {
+  const defaultColors = ["#a0a0a0", "#cccccc", "#a0a0a0"];
+
+  // Couleurs pour chaque bouton
+  const yellowColors = ["#ffcc00", "#ffe066", "#ffcc00"];
+  const blueColors = ["#0066ff", "#66a3ff", "#0066ff"];
+  const pinkColors = ["#ff3366", "#ff99aa", "#ff3366"];
+
+  // État pour suivre les couleurs actuelles
+  const [colors, setColors] = useState(defaultColors);
+
+  // Fonctions pour gérer le survol de chaque bouton
+  const handleYellowEnter = () => {
+    setColors(yellowColors);
+  };
+
+  const handleBlueEnter = () => {
+    setColors(blueColors);
+  };
+
+  const handlePinkEnter = () => {
+    setColors(pinkColors);
+  };
+
+  const handleMouseLeave = () => {
+    setColors(defaultColors);
+  };
+
   const { updateEmotion } = useEmotion();
   const navigate = useNavigate();
 
@@ -16,44 +44,50 @@ const Question1: React.FC = () => {
   };
 
   return (
-    <div className="question-container">
-      {/* <Aurora
-        colorStops={["#005050", "#808080", "#D3D3D3"]}
-        blend={0.5}
-        amplitude={0.5}
-        speed={0.5}
-      /> */}
-      <div className="emotions-buttons">
-        <button
-          onClick={() => handleChoice("joie")}
-          className="emotion-button joie joy"
-        >
-          <span className="emotion-emoji">😄</span>
-          <span className="emotion-label">Joie</span>
-        </button>
-        <button
-          onClick={() => handleChoice("tristesse")}
-          className="emotion-button tristesse sadness"
-        >
-          <span className="emotion-emoji">😢</span>
-          <span className="emotion-label">Tristesse</span>
-        </button>
-        <button
-          onClick={() => handleChoice("amour")}
-          className="emotion-button amour love"
-        >
-          <span className="emotion-emoji">❤️</span>
-          <span className="emotion-label">Amour</span>
-        </button>
+    <>
+      <div className="question-container">
+        <div className="app-container">
+          {/* Aurora en arrière-plan */}
+          <div className="background-aurora">
+            <Aurora colorStops={colors} />
+          </div>
+        </div>
+        <div className="emotions-buttons">
+          <button
+            onClick={() => handleChoice("joie")}
+            className="emotion-button joie joy"
+            onMouseEnter={handleYellowEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <span className="emotion-emoji">😄</span>
+            <span className="emotion-label">Joie</span>
+          </button>
+          <button
+            onClick={() => handleChoice("tristesse")}
+            className="emotion-button tristesse sadness"
+            onMouseEnter={handleBlueEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <span className="emotion-emoji">😢</span>
+            <span className="emotion-label">Tristesse</span>
+          </button>
+          <button
+            onClick={() => handleChoice("amour")}
+            className="emotion-button amour love"
+            onMouseEnter={handlePinkEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <span className="emotion-emoji">❤️</span>
+            <span className="emotion-label">Amour</span>
+          </button>
+        </div>
+
+        <div className="question-info">
+          <h2>Question 1</h2>
+          <p>Comment vous sentez-vous le matin au réveil ?</p>
+        </div>
       </div>
-      <div className="progress-bar">
-        <div className="progress" style={{ width: "33.3%" }}></div>
-      </div>
-      <div className="question-info">
-        <h2>Question 1</h2>
-        <p>Comment vous sentez-vous le matin au réveil ?</p>
-      </div>
-    </div>
+    </>
   );
 };
 
--- projet-crea-dev/src/context/Question2.tsx
+++ projet-crea-dev/src/context/Question2.tsx
@@ -3,10 +3,39 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useEmotion } from "../context/EmotionContext";
 import "../../public/styles/pages.css";
+import Aurora from "./Aurora";
+import { useState } from "react";
 
 import { Emotion } from "../types";
 
 const Question2: React.FC = () => {
+  const defaultColors = ["#a0a0a0", "#cccccc", "#a0a0a0"];
+
+  // Couleurs pour chaque bouton
+  const yellowColors = ["#ffcc00", "#ffe066", "#ffcc00"];
+  const blueColors = ["#0066ff", "#66a3ff", "#0066ff"];
+  const pinkColors = ["#ff3366", "#ff99aa", "#ff3366"];
+
+  // État pour suivre les couleurs actuelles
+  const [colors, setColors] = useState(defaultColors);
+
+  // Fonctions pour gérer le survol de chaque bouton
+  const handleYellowEnter = () => {
+    setColors(yellowColors);
+  };
+
+  const handleBlueEnter = () => {
+    setColors(blueColors);
+  };
+
+  const handlePinkEnter = () => {
+    setColors(pinkColors);
+  };
+
+  const handleMouseLeave = () => {
+    setColors(defaultColors);
+  };
+
   const { updateEmotion } = useEmotion();
   const navigate = useNavigate();
 
@@ -16,38 +45,50 @@ const Question2: React.FC = () => {
   };
 
   return (
-    <div className="question-container">
-      <div className="emotions-buttons">
-        <button
-          onClick={() => handleChoice("joie")}
-          className="emotion-button joie joy"
-        >
-          <span className="emotion-emoji">😄</span>
-          <span className="emotion-label">Joie</span>
-        </button>
-        <button
-          onClick={() => handleChoice("tristesse")}
-          className="emotion-button tristesse sadness"
-        >
-          <span className="emotion-emoji">😢</span>
-          <span className="emotion-label">Tristesse</span>
-        </button>
-        <button
-          onClick={() => handleChoice("amour")}
-          className="emotion-button amour love"
-        >
-          <span className="emotion-emoji">❤️</span>
-          <span className="emotion-label">Amour</span>
-        </button>
+    <>
+      <div className="question-container">
+        <div className="app-container">
+          {/* Aurora en arrière-plan */}
+          <div className="background-aurora">
+            <Aurora colorStops={colors} />
+          </div>
+        </div>
+        <div className="emotions-buttons">
+          <button
+            onClick={() => handleChoice("joie")}
+            className="emotion-button joie joy"
+            onMouseEnter={handleYellowEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <span className="emotion-emoji">😄</span>
+            <span className="emotion-label">Joie</span>
+          </button>
+          <button
+            onClick={() => handleChoice("tristesse")}
+            className="emotion-button tristesse sadness"
+            onMouseEnter={handleBlueEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <span className="emotion-emoji">😢</span>
+            <span className="emotion-label">Tristesse</span>
+          </button>
+          <button
+            onClick={() => handleChoice("amour")}
+            className="emotion-button amour love"
+            onMouseEnter={handlePinkEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <span className="emotion-emoji">❤️</span>
+            <span className="emotion-label">Amour</span>
+          </button>
+        </div>
+
+        <div className="question-info">
+          <h2>Question 2</h2>
+          <p>Quelle émotion ressentez-vous quand vous pensez à votre futur ?</p>
+        </div>
       </div>
-      <div className="progress-bar">
-        <div className="progress" style={{ width: "66.6%" }}></div>
-      </div>
-      <div className="question-info">
-        <h2>Question 2</h2>
-        <p>Quelle émotion ressentez-vous quand vous pensez à votre futur ?</p>
-      </div>
-    </div>
+    </>
   );
 };
 
